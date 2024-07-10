8ac7a8044bf5cae50d8e1a82e5de7032f6b63cf5
reijuu <loveminorin_3397@yahoo.co.jp>
2024-07-11 00:00:56 +0900

    コミット

--- src/main/java/reijuu/jisakuMod2/Item/Item/custom/JisakuArrowItem.java
+++ src/main/java/reijuu/jisakuMod2/Item/Item/custom/JisakuArrowItem.java
@@ -3,13 +3,14 @@ package reijuu.jisakuMod2.Item.Item.custom;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import reijuu.jisakuMod2.entity.projectile.JisakuArrow;
 
-public class JisakuArrowItem extends Item {
+public class JisakuArrowItem extends ArrowItem {
 
+    //itemからArrowItemに変更
     public JisakuArrowItem(Properties pProperties) {
         super(pProperties);
     }
--- src/main/java/reijuu/jisakuMod2/Item/Item/custom/JisakuBow.java
+++ src/main/java/reijuu/jisakuMod2/Item/Item/custom/JisakuBow.java
@@ -2,7 +2,9 @@ package reijuu.jisakuMod2.Item.Item.custom;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EntityType;
@@ -11,6 +13,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
 import reijuu.jisakuMod2.Item.JisakuItems;
@@ -45,42 +49,72 @@ public class JisakuBow extends BowItem  {
     }
 
     @Override
-    public void releaseUsing(ItemStack itemstack, Level world, LivingEntity entity, int time) {
-        if (!world.isClientSide() && entity instanceof ServerPlayer player) {
-            ItemStack ammo = findAmmo(player);
-            if (player.getAbilities().instabuild || !ammo.isEmpty()) {
-                AbstractArrow arrow;
-                if (ammo.getItem() instanceof JisakuArrowItem) {
-                    JisakuArrowItem jisakuArrowItem = (JisakuArrowItem) ammo.getItem();
-                    arrow = jisakuArrowItem.createArrow(world, ammo, player);
-                } else {
-                    arrow = new Arrow(EntityType.ARROW, world); // デフォルトの矢
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving, int pTimeLeft) {
+        //バニラのbowitemのソース
+        if (pEntityLiving instanceof Player player) {
+            boolean flag = player.getAbilities().instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, pStack) > 0;
+            ItemStack itemstack = findAmmo(player);
+
+            int i = this.getUseDuration(pStack) - pTimeLeft;
+            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(pStack, pLevel, player, i, !itemstack.isEmpty() || flag);
+            if (i < 0) return;
+
+            if (!itemstack.isEmpty() || flag) {
+                if (itemstack.isEmpty()) {
+                    itemstack = new ItemStack(JisakuItems.JISAKU_ARROW.get());
                 }
-                arrow.setBaseDamage(150.0);
-                arrow.shootFromRotation(entity, entity.getXRot(), entity.getYRot(), 0, 3.15f, 1.0F);
-                world.addFreshEntity(arrow);
 
-                world.playSound(null, player.getX(), player.getY(), player.getZ(),
-                        Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot"))),
-                        SoundSource.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.5F + 1.0F));
+                float f = getPowerForTime(i);
+                if (!((double)f < 0.1D)) {
+                    boolean flag1 = player.getAbilities().instabuild || (itemstack.getItem() instanceof ArrowItem && ((ArrowItem)itemstack.getItem()).isInfinite(itemstack, pStack, player));
+                    if (!pLevel.isClientSide) {
 
-                if (player.getAbilities().instabuild) {
-                    arrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
-                } else {
-                    if (ammo.isDamageableItem()) {
-                        if (ammo.hurt(1, world.getRandom(), player)) {
-                            ammo.shrink(1);
-                            ammo.setDamageValue(0);
-                            if (ammo.isEmpty()) {
-                                player.getInventory().removeItem(ammo);
-                            }
+                        //ここがなかったため矢が発射できない可能性
+                        ArrowItem arrowitem = (ArrowItem)(itemstack.getItem() instanceof ArrowItem ? itemstack.getItem() : Items.ARROW);
+                        AbstractArrow abstractarrow = arrowitem.createArrow(pLevel, itemstack, player);
+                        abstractarrow = customArrow(abstractarrow);
+                        abstractarrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 3.0F, 1.0F);
+
+                        //矢のダメージ量
+                        abstractarrow.setBaseDamage(1500);
+
+                        if (f == 1.0F) {
+                            abstractarrow.setCritArrow(true);
                         }
-                    } else {
-                        ammo.shrink(1);
-                        if (ammo.isEmpty()) {
-                            player.getInventory().removeItem(ammo);
+
+                        int j = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.POWER_ARROWS, pStack);
+                        if (j > 0) {
+                            abstractarrow.setBaseDamage(abstractarrow.getBaseDamage() + (double)j * 0.5D + 0.5D);
+                        }
+
+                        int k = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, pStack);
+                        if (k > 0) {
+                            abstractarrow.setKnockback(k);
+                        }
+
+                        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, pStack) > 0) {
+                            abstractarrow.setSecondsOnFire(100);
+                        }
+
+                        pStack.hurtAndBreak(1, player, (p_296888_) -> {
+                            p_296888_.broadcastBreakEvent(player.getUsedItemHand());
+                        });
+                        if (flag1 || player.getAbilities().instabuild && (itemstack.is(Items.SPECTRAL_ARROW) || itemstack.is(Items.TIPPED_ARROW))) {
+                            abstractarrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+                        }
+
+                        pLevel.addFreshEntity(abstractarrow);
+                    }
+
+                    pLevel.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F / (pLevel.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!flag1 && !player.getAbilities().instabuild) {
+                        itemstack.shrink(1);
+                        if (itemstack.isEmpty()) {
+                            player.getInventory().removeItem(itemstack);
                         }
                     }
+
+                    player.awardStat(Stats.ITEM_USED.get(this));
                 }
             }
         }
